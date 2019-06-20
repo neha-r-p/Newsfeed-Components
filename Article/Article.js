69c1bfcd4a7d629db25c9fc15cf7a1a09bb2f6e0
Neha Patel <nehatr0n@Nehas-MacBook-Pro.local>
2019-06-19 19:32:42 -0700

    toggle article set, but height is restricting

--- Article/Article.js
+++ Article/Article.js
@@ -9,14 +9,14 @@ class Article {
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = "expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', this.expandArticle);
+    this.expandButton.addEventListener('click', this.expandArticle.bind(this));
     
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    console.log("I was clicked");
-
+    // console.log("I was clicked");
+this.domElement.classList.toggle('article-open');
   }
 }
 
