0311a5ee96b6b1eefaf89c430920e5c9278345b8
Neha Patel <nehatr0n@Nehas-MacBook-Pro.local>
2019-06-19 17:54:54 -0700

    event listener added to expandButton

--- Article/Article.js
+++ Article/Article.js
@@ -9,11 +9,13 @@ class Article {
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = "expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
+    this.expandButton.addEventListener('click', this.expandArticle);
     
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
+    console.log("I was clicked");
 
   }
 }
