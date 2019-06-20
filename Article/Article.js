2f34e3db50b8f786cf56274d4a946d63e9f3bd9f
Neha Patel <nehatr0n@Nehas-MacBook-Pro.local>
2019-06-19 20:15:09 -0700

    MVP

--- Article/Article.js
+++ Article/Article.js
@@ -10,7 +10,6 @@ class Article {
     this.expandButton.textContent = "expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', this.expandArticle.bind(this));
-    
   }
 
   expandArticle() {
